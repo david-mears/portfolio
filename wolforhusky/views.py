361ea76c4575196edb0f385e878e3ae480114dc4
David Mears <kieukmail@gmail.com>
2019-07-30 17:06:35 +0100

    Refactor: Improve naming and readability

--- wolforhusky/views.py
+++ wolforhusky/views.py
@@ -9,12 +9,11 @@ def index(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             image_obj = form.save()
-            image_filepath = image_obj.image.path
             model_path = os.path.join(
                 os.path.dirname(os.path.abspath(__file__)),
                 'learning_models',
             )
-            prediction = load_and_predict(image_filepath, model_path)
+            prediction = get_prediction(image_obj.image.path, model_path)
             predicted_class = str(prediction[0]).replace('_', ' ')
             predicted_class_index = prediction[1].item()
             confidence = int(100*prediction[2][predicted_class_index].item())  
@@ -31,7 +30,7 @@ def index(request):
     }
     return render(request, 'wolforhusky_index.html', context)
 
-def load_and_predict(image_filepath, model_path):
+def get_prediction(image_filepath, model_path):
     learn = basic_train.load_learner(model_path)
     image_for_fastai = vision.image.open_image(image_filepath)
     return learn.predict(image_for_fastai)
